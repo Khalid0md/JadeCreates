c741710bfe7a3ce4da189ac16a10b5d0363afb33
khalid0md <khalid.abdallas.here@gmail.com>
2022-04-12 13:08:40 -0300

    final testing

--- backend/config.js
+++ backend/config.js
@@ -1,3 +1,3 @@
-export const marketplaceAddress = "0xBf7738490D8B74E373977b35FcFDa7cD7794Ae9b"
-export const storeMarketplaceAddress = "0xe5A391101a5bAD570e329330b146Eb5acCaC0B4e"
+export const marketplaceAddress = "0xD1fe74f09f98773B5293e07BF8C6F5Bb3d89e3Ec"
+export const storeMarketplaceAddress = "0x36F95d2713Ba60FAa97E619C505bF66b63A49864"
 export const nftAddress = "0xd2a4C5461E1aaF74Cb1FD68090CAD4e7aA5F3Abc"
